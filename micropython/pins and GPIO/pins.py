dbf04e4bd4394ea856f798b73dc0dd0cb2be3dbb
WashingtonKK <washingtonkigan@gmail.com>
2020-09-10 02:16:59 +0300

    GPIO pins

---
+++ micropython/pins and GPIO/pins.py
@@ -0,0 +1,18 @@
+from machine import Pin
+
+p0 = Pin(0, Pin.OUT)    # create output pin on GPIO0
+p0.on()                 # set pin to "on" (high) level
+p0.off()                # set pin to "off" (low) level
+p0.value(1)             # set pin to on/high
+
+p2 = Pin(2, Pin.IN)     # create input pin on GPIO2
+print(p2.value())       # get value, 0 or 1
+
+# enable internal pull-up resistor
+p4 = Pin(4, Pin.IN, Pin.PULL_UP)
+
+# set pin high on creation
+p5 = Pin(5, Pin.OUT, value=1) 
+
+while True:
+    #Input your code here
